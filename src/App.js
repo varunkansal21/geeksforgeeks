b549562cc421bdb31ac80b00aeab0071a1718329
Varun Kansal <varunkansal2111@gmail.com>
2022-12-24 17:40:20 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Home from './components/Home';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Add from './components/Add';
